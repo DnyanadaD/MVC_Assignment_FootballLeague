77cfbac041e308a37b76309793e5fac2538d59f0
DnyanadaD <dnyanada.dhore@gmail.com>
2022-12-05 22:59:06 +0530

    first commit

---
+++ FootBallLeagueManagement/Controllers/FootballLeagueController.cs
@@ -0,0 +1,40 @@
+﻿using FootBallLeagueManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootBallLeagueManagement.Controllers
+{
+    public class FootballLeagueController : Controller
+    {
+        private readonly FootBallLeagueDbContext _context;
+        public FootballLeagueController(FootBallLeagueDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<FootballLeague> objCatlist = _context.FootballLeagues;
+            return View(objCatlist);
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(FootballLeague Football)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.FootballLeagues.Add(Football);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Record Added Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(Football);
+        }
+    }
+}
+
---
+++ FootBallLeagueManagement/Models/FootBallLeagueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FootBallLeagueManagement.Models
+{
+    public class FootBallLeagueDbContext :DbContext
+    {
+        public FootBallLeagueDbContext(DbContextOptions<FootBallLeagueDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FootballLeague> FootballLeagues { get; set; }
+    }
+}
